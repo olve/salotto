91e875d4f729e0c00a62dc0bc73cf8defdf3b2f2
Olve <oisevb@gmail.com>
2017-02-06 13:59:03 +0100

    added mainmenu logo
    reconsider the slanted admin-text.

--- cms/src/components/MainMenu/index.jsx
+++ cms/src/components/MainMenu/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import TextFieldsIcon from 'material-ui/svg-icons/editor/text-fields'
 import CollectionsIcon from 'material-ui/svg-icons/image/collections'
 
+import SalottoLogoSvg from '../SalottoLogoSvg'
 import Card from './Card'
 import './styles.css'
 
@@ -9,14 +10,16 @@ export default class MainMenu extends React.Component {
   render() {
     return (
       <div id="mainmenu">
-        <div className="aligner-item--top"></div>
+
+        <div id="mainmenu-logo-wrap">
+          <SalottoLogoSvg />
+          <p>Admin</p>
+        </div>
 
         <div className="cards">
           <Card title="select background images" href="images" icon={CollectionsIcon} />
           <Card title="Edit text in about-section" href="about" icon={TextFieldsIcon} />
         </div>
-
-        <div className="aligner-item--bottom"></div>
 
       </div>
     )
