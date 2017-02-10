3e2f6c818d63f7b7939710e217c1bb32745333b8
Olve <oisevb@gmail.com>
2017-02-10 14:01:33 +0100

    make requests with fetch

--- cms/src/components/ImagePicker/index.jsx
+++ cms/src/components/ImagePicker/index.jsx
@@ -1,36 +1,31 @@
 import React from 'react'
 
+import fetcher from '../../fetcher'
+
 export default class ImagePicker extends React.Component {
   constructor() {
     super()
     this.apiImagesUrl = `${process.env.CMS_API_PROTO}://${process.env.CMS_API_HOST}:${process.env.CMS_API_PORT}${process.env.CMS_API_ROOT}/images`
   }
-  get() {
-    const xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        alert(xhr.responseText)
-      }
-    }
-    xhr.open('GET', this.apiImagesUrl, true)
-    xhr.send()
+  getImages() {
+    fetcher.get(this.apiImagesUrl).then(({message}) => {
+      console.log(message)
+    })
   }
-  post() {
-    const xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        alert(xhr.responseText)
-      }
-    }
-    xhr.open('POST', this.apiImagesUrl, true)
-    xhr.send()
+  setImages(images) {
+    fetcher.post(this.apiImagesUrl, images ? JSON.stringify(images) : null).then(({message}) => {
+      console.log(message)
+    })
+  }
+  test(event) {
+    this.setImages(['1.jpg', '2.jpg'])
   }
   render() {
     return (
       <div>
         <p>ImagePicker</p>
-        <button onClick={this.get.bind(this)}>get</button>
-        <button onClick={this.post.bind(this)}>post</button>
+        <button onClick={this.getImages.bind(this)}>get</button>
+        <button onClick={this.test.bind(this)}>post</button>
       </div>
     )
   }
---
+++ cms/src/fetcher.js
@@ -0,0 +1,24 @@
+
+const exports = {}
+
+exports.handleErrors = (response) => {
+  if (!response.ok) {
+    throw Error(response.statusText)
+  }
+  return response
+}
+exports.parseResponse = (response) => response.text().then(text => JSON.parse(text))
+
+exports.get = (url) => fetch(url, {method: 'GET'})
+                                .then(exports.handleErrors)
+                                .then(response => exports.parseResponse(response))
+                                .catch(error => {
+                                  console.log(error)
+                                })
+exports.post = (url, body) => fetch(url, {method: 'POST', body,})
+                                .then(exports.handleErrors)
+                                .then(response => exports.parseResponse(response))
+                                .catch(error => {
+                                  console.log(error)
+                                })
+export default exports
